af6bc3c2f43ee24105e84721f35dbdf88252b452
pngigena <pngigena@gmail.com>
2018-10-08 23:39:00 -0300

    Aprender a indentar

---
+++ Identacion.py
@@ -0,0 +1,6 @@
+for i in range(2):
+        if i % 2 == 0:
+            print("Estoy dentro del bloque if, por lo que...+")
+            print(i, "es un numero par.\n")
+        print("El bloque if se ha omitido, por lo que...+")
+        print(i, " es un nro impar\n")
