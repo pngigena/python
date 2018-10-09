36c4874d5a0d39cc6dbb394882ab7c2f0e3c5237
pngigena <pngigena@gmail.com>
2018-10-08 23:59:02 -0300

    Tipos de datos

---
+++ tipos_dato.py
@@ -0,0 +1,18 @@
+""" Numeros:
+Pueden ser del tipo int, float, boolean y complex
+Conuntos del tipo set
+secuencias de cadenas : str, byte , list , tuple
+diccionarios: dict"""
+
+i= 1
+j= 2
+#Imprime el tipo de dato
+print(type(i))
+
+
+""" Cada objeto tiene una identidad , ademas de sus atributos"""
+print(id(i))
+
+print(id(j))
+
+"""Los atributos se ven con el punto j.(atributo)"""
